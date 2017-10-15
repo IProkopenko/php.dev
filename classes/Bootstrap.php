96aebd13d7574132b8ae3b0b1446e7550eb5a4c3
iprokopenko <iryna.prokopenko@theiconic.com.au>
2017-10-16 09:53:14 +1100

    before creatin base controller

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -10,17 +10,44 @@ class Bootstrap
     {
         $this->request = $request;
 
-        if( $this->request['controller'] == ""){
+        if ($this->request['controller'] == "") {
             $this->controller = 'home';
         } else {
             $this->controller = $this->request['controller'];
         }
 
-        if($this->request['action'] == ""){
+        if ($this->request['action'] == "") {
             $this->action = 'index';
         } else {
             $this->action = $this->request['controller'];
         }
+
         echo $this->controller;
+    }
+
+    public function createController()
+    {
+        //Check Class
+        if (class_exists($this->controller)) {
+            $parents = class_parents($this->controller);
+
+            //check Extend
+            //in_array("Glenn", $array)
+            if (in_array('Controller', $parents)) {
+                if (method_exists($this->controller, $this->action)) {
+                    return new $this->controller($this->action, $this->request);
+                } else {
+                    // method doesnt exist
+                    echo '<h1>Method does not exist</h1>';
+                    return;
+                }
+            } else {
+                echo '<h1>Base controller does not exist</h1>';
+                return;
+            }
+        } else {
+            echo '<h1>Controller Class does not exist</h1>';
+            return;
+        }
     }
 }
